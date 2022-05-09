7ca7f548aab32fbe2d9d780f13e0925b3a34bdd8
Rodrigo Oportus <r.opreller@gmail.com>
2022-05-09 18:30:22 -0400

    Segunda entrega

--- app.js
+++ app.js
@@ -26,13 +26,18 @@ Vue.component('primer-comp', {
     }
 })
 
-Vue.component('producto', {
+
+Vue.component('producto2', {
     data() {
         return {
 
         }
     },
     props: {
+        id: {
+            type: Number,
+            required: true
+        },
         name: {
             type: String,
             required: true
@@ -48,47 +53,84 @@ Vue.component('producto', {
         img: {
             type: String,
             required: true
+        },
+        stock: {
+            type: Number,
+            required: true
+        },
+        addBtn: {
+            type: Boolean
         }
     },
     template:
     `
-    <div class="productContent">
-        <div class="card" style="width: 18rem;">
-            <img :src="img" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{ name }}</h5>
-                <h3 class="card-title">$ {{ price }}</h3>
-                <p class="card-text">{{ desc }}</p>
-                <a href="#" class="btn btn-primary">Ver</a>
-            </div>
-        </div>
-    </div>
+    <tr v-bind:class="{ tdNoDisponible: !addBtn }">>
+    <th scope="row">{{ id }}</th>
+    <td>$ {{ price }}</td>
+    <td><img :src="img" class="card-img-top imgTabla" :alt="name"></td>
+    <td>{{ name }}</td>
+    <td>{{ desc }}</td>
+    <td>{{ stock }}</td>
+    <td>
+    <button v-if="addBtn" type="button" class="btn btn-success">Añadir</button>
+    <p v-else="addBtn" class="noDisponible">No disponible</p>
+    </td>
+    </tr>
     `
 })
 
-ppp
 
 var app = new Vue({
     el: "#app",
     data: {
         products: [
             {
+                id: 1,
                 name: 'Churu clásico unidad',
                 price: 1500,
                 desc: 'Clásico churu sabor atún.',
-                img: './img/churu.jpg'
+                img: './img/churu.jpg',
+                stock: 2
             },
             {
+                id: 2,
                 name: 'Bravery 7kg.',
                 price: 45000,
                 desc: 'Alimento Bravery sabor salmón.',
-                img: './img/bravery.jpg'
+                img: './img/bravery.jpg',
+                stock: 0
             },
             {
+                id: 3,
                 name: 'Matisse 7kg.',
                 price: 43200,
                 desc: 'Alimento Mattise sabor salmón.',
-                img: './img/mattise.jpg'
+                img: './img/mattise.jpg',
+                stock: 3
+            },
+            {
+                id: 4,
+                name: 'Leonardo lata.',
+                price: 3450,
+                desc: 'Alimento humedo marca exclusiva.',
+                img: './img/leonardo.jpg',
+                stock: 7
+            },
+            {
+                id: 5,
+                name: 'Leonardo kitten.',
+                price: 46200,
+                desc: 'Leonardo kitten sabor inigualable.',
+                img: './img/Leonardo_2.jpg',
+                stock: 0
+            },
+            {
+                id: 6,
+                name: 'Proplan urinary.',
+                price: 33600,
+                desc: 'Para el cuidado de sus riñones.',
+                img: './img/proplan_urinary.jpg',
+                stock: 1
             }
         ]
     },
@@ -103,15 +145,33 @@ var app = new Vue({
     template: // Puede almacenar otros componentes
         `
         <div>
-        <h1> Elemento tomado desde template #app</h1>
+        <h1> Alimentos de gato a la venta</h1>
         <br>
-        <primer-comp />
-        <producto v-for="(item, i) in products" :key="i"
-            :name="item.name"
-            :price="item.price"
-            :desc="item.desc"
-            :img="item.img"
-        />
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Foto</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Añadir</th>
+                </tr>
+            </thead>
+            <tbody>
+            <producto2 v-for="(item, i) in products"
+                :key="i"
+                :id="item.id"
+                :name="item.name"
+                :price="item.price"
+                :desc="item.desc"
+                :img="item.img"
+                :stock="item.stock"
+                :addBtn="item.stock > 0"
+            />
+            </tbody>
+        </table>
         </div>
     `
 })
